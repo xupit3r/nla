f117282d5f144f71bcce474c4b1ce52b9d47fe3d
Joe D'Alessandro <joe@thejoeshow.net>
2023-03-13 03:43:41 -0400

    first commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject nla "1.0.0"
+  :description "working through Numerical Linear Algebra for Programmers book"
+  :url "https://github.com/xupit3r/nla"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :repl-options {:init-ns nla.core})
